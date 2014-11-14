ccd95c1321f14523c95edb299c034371fe40475e
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2014-11-14 20:27:20 +0100

    Restyled hover menus, fixed menus.js

--- tecweb/esercizi/dynamic-menus/scripts/menus.js
+++ tecweb/esercizi/dynamic-menus/scripts/menus.js
@@ -4,20 +4,20 @@
 
 var menuHoriz = $('#menuOne li');
 var menuVert = $('.menuVert');
+var subMenus = $('#subMenus');
 for (var i=0; i<menuHoriz.length; ++i) {
     menuHoriz[i].addEventListener('mouseover', function() {
         var index1 = $(this).index();
-        $(menuVert[index1]).css('margin-left', index1*74 + 'px');
         $(menuVert[index1]).css('display', 'inline-block');
-    });
-    menuHoriz[i].addEventListener('mouseout', function() {
-        var index2 = $(this).index();
-        $(menuVert[index2]).css('display', 'none');
-    });
-    menuVert[i].addEventListener('mouseover', function() {
-        $(this).css('display', 'inline-block');
-    });
-    menuVert[i].addEventListener('mouseout', function() {
-        $(this).css('display', 'none');
+        for (var j=0; j<menuVert.length; j++) {
+            if (j!=index1) {
+                $(menuVert[j]).css('display', 'none');
+            }
+        }
+        $(subMenus).css('display', "inline-block");
     });
 }
+$('header, #container').on('mouseover', function() {
+   $(subMenus).css('display', 'none');
+    console.log('mouseout');
+});
